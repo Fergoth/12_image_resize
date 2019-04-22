10547dcc7ead3e3de51bb0acd1580ba194dec0f4
Fergoth <fergothew@gmail.com>
2019-04-22 09:07:49 +0300

    added readme

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # Image Resizer
+Resize image to new sizes.
+# Description
+You can give new height or new width of image (or both, or scale) that will be rescaled to new width and height
+You also can give path to where you want to save new rescaled image (as default such path as path to rescaling image)
+New image save as "[pic_name]__[new_width]x[new_height].jpg"
+# Example
+```bash
+C:/12_image_resize/image_resize.py default.jpg -h
+usage: image_resize.py [-h] [-ph HEIGHT] [-pw WIDTH] [-ps SCALE] [-out OUTPUT]
+                       path_to_image
 
-[TODO. There will be project description]
+positional arguments:
+  path_to_image         Path to converting image
 
+optional arguments:
+  -h, --help            show this help message and exit
+  -ph HEIGHT, --height HEIGHT
+                        New image height
+  -pw WIDTH, --width WIDTH
+                        New image width
+  -ps SCALE, --scale SCALE
+                        Scale for resizing image
+  -out OUTPUT, --output OUTPUT
+                        path where we save rescaling image
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- image_resize.py
+++ image_resize.py
@@ -9,8 +9,8 @@ def save_image(image, args):
     new_image_name = name + "__{}x{}".format(*image.size) + ending
     if args.output:
         if os.path.isdir(args.output):
-            os.path.join(args.output,new_image_name)
-            image.save(args.output+"__")
+            os.path.join(args.output, new_image_name)
+            image.save(args.output + "__")
         else:
             print('Путь не является папкой')
     else:
@@ -36,28 +36,31 @@ def get_image(path):
     image = Image.open(path)
     return image
 
+
 def scale_new_sizes(width, height, scale):
-    return width*scale, height*scale
+    return width * scale, height * scale
+
 
 def print_warning(new_proportion, old_proportion):
     if new_proportion != old_proportion:
         print('Пропорции не совпадают с исходными!')
 
+
 def get_new_width_height(args, image):
     width, height = image.size
-    old_proportion = width/height
+    old_proportion = width / height
     if (args.width or args.height) and args.scale:
         return None
     if args.scale:
         return scale_new_sizes(width, height, args.scale)
     if args.width and args.height:
         new_proportion = args.width / args.height
-        print_warning(new_proportion,old_proportion)
+        print_warning(new_proportion, old_proportion)
         return args.width, args.height
     if args.height:
-        return scale_new_sizes(width, height, args.height/height)
+        return scale_new_sizes(width, height, args.height / height)
     if args.width:
-        return scale_new_sizes(width, height, args.width/width)
+        return scale_new_sizes(width, height, args.width / width)
 
 
 if __name__ == '__main__':
@@ -73,4 +76,3 @@ if __name__ == '__main__':
         save_image(new_image, args)
     else:
         print("Требуется (Высота и(или) Ширина) или Маштаб")
-
